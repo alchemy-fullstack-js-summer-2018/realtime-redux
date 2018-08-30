183196fca785305b4598e3862ebaf93b6be074dd
Mark Myers <markalope@gmail.com>
2018-08-30 09:39:33 -0700

    more file work, this time in functions folder

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.database();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
---
+++ src/components/app/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import styles from './Header.css';
+
+class Header extends Component {
+
+  handleSpecial = event => {
+    event.preventDefault();
+    alert('what the heck i=does this do?');
+  };
+
+  render() {
+    return (
+      <div className={styles.header}>
+      
+        <section className="header-container">
+          <div className="logo">
+            <img src={logo}/>
+            <h1>Expenses by Budget Category</h1>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink exact activeClassName ="active" to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink exact activeClassName ="active" to="/categories">Categories</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Header;
