7fc0dcaee351ab7bca12673e7f7c53323eb7fc1b
Mark Myers <markalope@gmail.com>
2018-08-30 09:46:54 -0700

    creating the base store file

---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import promiseMiddleware from './promise-middleware';
+import { error, loading, user, games } from '../components/app/reducers';
+import { game } from '../components/game/reducers';
+
+const rootReducer = combineReducers({
+  error, loading, user, games, game
+});
+
+const composeEnhancers = window.__REDUXDEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk,
+      promiseMiddleware
+    )
+  )
+);
+
+export default store;
